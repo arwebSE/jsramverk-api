e82b093468910a90f5ff0661961251f48c1fbd67
August R <aggesoft@gmail.com>
2020-08-24 17:57:23 +0200

    test routes

--- app.js
+++ app.js
@@ -3,10 +3,46 @@ const app = express();
 
 const port = 1337;
 
-// Add a route
+// Index route
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("API check ok!");
+});
+
+// Testing routes with method
+app.get("/user", (req, res) => {
+    res.json({
+        data: {
+            msg: "Got a GET request"
+        }
+    });
+});
+
+// Testing routes with method
+app.get("/user", (req, res) => {
+    res.json({
+        data: {
+            msg: "Got a GET request, sending back default 200"
+        }
+    });
+});
+
+app.post("/user", (req, res) => {
+    res.status(201).json({
+        data: {
+            msg: "Got a POST request, sending back 201 Created"
+        }
+    });
+});
+
+app.put("/user", (req, res) => {
+    // PUT requests should return 204 No Content
+    res.status(204).send();
+});
+
+app.delete("/user", (req, res) => {
+    // DELETE requests should return 204 No Content
+    res.status(204).send();
 });
 
 // Start up server
-app.listen(port, () => console.log(`Example API listening on port ${port}!`));
+app.listen(port, () => console.log(`Running API on port ${port}!`));
