cc0d76b5f7a4d846e59e06dfc25d9b62127bb098
August R <aggesoft@gmail.com>
2020-08-25 13:40:47 +0200

    halfway done kmom01

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-const express = require("express");
-const app = express();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require("body-parser");
 
+const app = express();
 const port = 1337;
 
-// Index route
-app.get("/", (req, res) => {
-    res.send("API check ok!");
-});
+const index = require('./routes/index');
+const hello = require('./routes/hello');
+
+app.use(cors());
+
+// don't show the log when it is test
+if (process.env.NODE_ENV !== 'test') {
+    // use morgan to log at command line
+    app.use(morgan('dev')); // 'combined' outputs the Apache style LOGs
+}
+
+app.use('/', index);
+app.use('/hello', hello);
 
 // Testing routes with method
 app.get("/user", (req, res) => {
@@ -44,5 +56,32 @@ app.delete("/user", (req, res) => {
     res.status(204).send();
 });
 
+// Add routes for 404 and error handling
+// Catch 404 and forward to error handler
+// Put this last
+app.use((req, res, next) => {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    if (res.headersSent) {
+        return next(err);
+    }
+
+    res.status(err.status || 500).json({
+        "errors": [
+            {
+                "status": err.status,
+                "title":  err.message,
+                "detail": err.message
+            }
+        ]
+    });
+});
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 // Start up server
 app.listen(port, () => console.log(`Running API on port ${port}!`));
---
+++ routes/hello.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/hello/:msg', function(req, res, next) {
+    const data = {
+        data: {
+            msg: req.params.msg
+        }
+    };
+    res.json(data);
+});
+
+module.exports = router;
